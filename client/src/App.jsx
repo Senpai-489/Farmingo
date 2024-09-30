2ce6eeda17e9197430c8d325382ab2cb643bea79
Amit Singh <amit489@gmail.com>
2024-09-30 13:56:54 +0530

    Landing_page_amit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Auth from './pages/auth'
 import Profile from './pages/profile'
+import Landing from './pages/Landing/Landing'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
+    <Route path="/" element={<Landing/>}/>
     <Route path="/auth" element={<Auth/>}/>
     <Route path="/profile" element={<Profile/>}/>
     <Route path='*' element={<Navigate to="auth"/>}/>
