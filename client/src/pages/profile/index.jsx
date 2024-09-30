c90a705ca62eb8a31251d10fb1c372e519ac83eb
krishnaGauss <goswamikrishna549@gmail.com>
2024-10-01 00:11:07 +0530

    Profile completed. Authorisation route completed. (Kurkure)

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,28 +1,30 @@
-import { useAppStore } from "@/store"
+import { useAppStore } from "@/store";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {MapContainer, TileLayer, Marker, useMapEvents} from "react-leaflet"
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import { toast } from "sonner";
+import { UPDATE_PROFILE_ROUTE } from "@/utils/constants";
+import { apiClient } from "@/lib/api-client";
+import { IoArrowBack } from "react-icons/io5";
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
-  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
 });
-
 
 const Profile = () => {
   const navigate = useNavigate();
-  const {userInfo, setUserInfo}=useAppStore();
-  const [firstName, setFirstName]=useState("");
-  const [lastName, setLastName]=useState("");
-  const [areaOfLand, setAreaOfLand]=useState(0);
+  const { userInfo, setUserInfo } = useAppStore();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [areaOfLand, setAreaOfLand] = useState(0);
   const [location, setLocation] = useState(null); // To store location coordinates
 
-  
   const validateProfile = () => {
     if (!firstName) {
       toast.error("First name is required.");
@@ -36,14 +38,14 @@ const Profile = () => {
       toast.error("Area of land is required.");
       return false;
     }
-    if(!location){
+    if (!location) {
       toast.error("Set location on map.");
       return false;
     }
-    
+
     return true;
   };
-  
+
   const handleNavigate = () => {
     if (userInfo.profileSetup) {
       navigate("/dashboard");
@@ -51,18 +53,27 @@ const Profile = () => {
       toast.error("Please setup profile first.");
     }
   };
-  
-  const saveChanges = async()=>{
-    if (validateProfile()) {
-        console.log(location)
 
+  const saveChanges = async () => {
+    if (validateProfile()) {
+      console.log({ location });
+      try {
+        const response = await apiClient.post(
+          UPDATE_PROFILE_ROUTE,
+          { firstName, lastName, areaOfLand, location },
+          { withCredentials: true }
+        );
+        if (response.status === 200 && response.data) {
+          setUserInfo({ ...response.data });
           toast.success("Profile updated successfully.");
           navigate("/dashboard");
         }
-      
+      } catch (err) {
+        console.log({ err });
+      }
     }
-  
-  
+  };
+
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -75,34 +86,36 @@ const Profile = () => {
         }
       );
     }
-  }, []); 
+  }, []);
   // Component for handling map clicks to select the farmer's location
   function LocationMarker() {
     useMapEvents({
       click(e) {
         const { lat, lng } = e.latlng; // Get latitude and longitude on click
-        setLocation([lat, lng]);       // Set the location state
-      }
+        setLocation([lat, lng]); // Set the location state
+      },
     });
 
     // Render a Marker if the location is set
-    return location === null ? null : (
-      <Marker position={location}></Marker>
-    );
-
+    return location === null ? null : <Marker position={location}></Marker>;
   }
 
   return (
     <div className="mx-auto mt-20 mb-10 p-4 shadow-2xl border-white rounded-3xl xl:w-[50vw] l:w-[50vw] md:w-[60vw] flex flex-col items-center justify-center overflow-auto hide-scrollbar">
       <div className=" h-[15vh] w-[15vw] absolute top-0 left-0 p-2">
-        <img src="logo_svg.svg"/>
+        <img src="logo_svg.svg" />
       </div>
-
-      <form className="space-y-4 h-[110vh] flex flex-col items-center justify-center ">
-      <h2 className="text-3xl font-bold mb-6 my-2">Farmer Profile</h2>
+      <div onClick={handleNavigate} className="">
+        <IoArrowBack className="text-4xl lg:text-5xl text-[#365170]/90 cursor-pointer" />
+      </div>
+      <div className="space-y-4 h-[110vh] flex flex-col items-center justify-center ">
+        <h2 className="text-3xl font-bold mb-6 my-2">Farmer Profile</h2>
         {/* First Name Input */}
         <div>
-          <label className="block text-gray-700 text-m font-bold mb-2" htmlFor="firstName">
+          <label
+            className="block text-gray-700 text-m font-bold mb-2"
+            htmlFor="firstName"
+          >
             First Name
           </label>
           <input
@@ -118,7 +131,10 @@ const Profile = () => {
 
         {/* Last Name Input */}
         <div>
-          <label className="block text-gray-700 text-m font-bold mb-2" htmlFor="lastName">
+          <label
+            className="block text-gray-700 text-m font-bold mb-2"
+            htmlFor="lastName"
+          >
             Last Name
           </label>
           <input
@@ -134,7 +150,10 @@ const Profile = () => {
 
         {/* Area of Land Input (in hectares) */}
         <div>
-          <label className="block text-gray-700 text-m font-bold mb-2" htmlFor="areaOfLand">
+          <label
+            className="block text-gray-700 text-m font-bold mb-2"
+            htmlFor="areaOfLand"
+          >
             Area of Land (in hectares)
           </label>
           <input
@@ -152,7 +171,11 @@ const Profile = () => {
           <label className="block text-gray-700 text-m font-bold mb-2">
             Select Location on Map (If Not automatically enabled)
           </label>
-          <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '20rem', width: '40rem' }}>
+          <MapContainer
+            center={[51.505, -0.09]}
+            zoom={13}
+            style={{ height: "20rem", width: "40rem" }}
+          >
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -160,7 +183,9 @@ const Profile = () => {
             <LocationMarker />
           </MapContainer>
           <p className="text-gray-600 text-m mt-2">
-            {location ? `Selected Coordinates: Latitude: ${location[0]}, Longitude: ${location[1]}` : 'Click on the map to select your location.'}
+            {location
+              ? `Selected Coordinates: Latitude: ${location[0]}, Longitude: ${location[1]}`
+              : "Click on the map to select your location."}
           </p>
         </div>
 
@@ -168,14 +193,15 @@ const Profile = () => {
         <div>
           <button
             type="submit"
-            className="w-[20rem] bg-blue-500 text-white font-bold text-lg py-2 px-4 rounded-lg hover:bg-blue-700" onClick={saveChanges}
+            className="w-[20rem] bg-blue-500 text-white font-bold text-lg py-2 px-4 rounded-lg hover:bg-blue-700"
+            onClick={saveChanges}
           >
             Submit
           </button>
         </div>
-      </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
-import { getUserInfo, login, signup } from "../controllers/AuthController.js";
+import { getUserInfo, login, signup, updateProfile } from "../controllers/AuthController.js";
 import { verifyToken } from "../middlewares/AuthMiddleware.js";
-
-
 
 const authRoutes = Router();
 
-authRoutes.post("/signup", signup)
-authRoutes.post("/login", login)
-authRoutes.get("/user-info", verifyToken, getUserInfo)
-
+authRoutes.post("/signup", signup);
+authRoutes.post("/login", login);
+authRoutes.get("/user-info", verifyToken, getUserInfo);
+authRoutes.post("/update-profile", verifyToken, updateProfile);
 
 export default authRoutes;
