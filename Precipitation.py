e09d7b4f13afb4f3afaf30286cec7b6a89e2bfdf
DigvijayDutt <duttdigvijay@gmail.com>
2024-10-03 18:54:05 +0530

    skeleton model

--- Precipitation.py
+++ Precipitation.py
@@ -1,6 +1,16 @@
-import requests
+from IPython.display import display 
 import pandas as pd
 import numpy as np
 import h5py as hdf
-
-file = hdf.File()
+from sklearn.linear_model import LinearRegression 
+with hdf.File('data.HDF5', 'r') as f:
+    data_keys = list(f.keys())
+    data= f.get('Grid')
+    dataset = data.get('precipitation')
+    df=pd.DataFrame(np.array(dataset))
+    
+TrainSet=[]
+TrainOp=[]
+predictor = LinearRegression()
+predictor.fit(X=TrainSet, y=TrainOp)
+predictor.predict(X=[[]])
