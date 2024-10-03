7a9f08e6e843f0ac8a03e93f13c4e2f624009fa2
DigvijayDutt <duttdigvijay@gmail.com>
2024-10-03 23:23:21 +0530

    ver2

--- Precipitation.py
+++ Precipitation.py
@@ -3,6 +3,14 @@ import pandas as pd
 import numpy as np
 import h5py as hdf
 from sklearn.linear_model import LinearRegression 
+import requests as rq
+from requests.auth import HTTPBasicAuth
+
+rq.get(
+    "https://arthurhouhttps.pps.eosdis.nasa.gov/trmmdata/ByDate/V07/2019/03/",
+    auth=HTTPBasicAuth("duttdigvijay@gmail.com", "duttdigvijay@gmail.com")
+)
+
 with hdf.File('data.HDF5', 'r') as f:
     data_keys = list(f.keys())
     data= f.get('Grid')
