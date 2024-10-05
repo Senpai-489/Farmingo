4f9fa44c83ac2b91546f84ab47b53b47c75721e2
DigvijayDutt <duttdigvijay@gmail.com>
2024-10-05 16:00:24 +0530

    ML Model

--- Data_Processing.py
+++ Data_Processing.py
@@ -17,34 +17,34 @@ data2 = bs(data1.text, 'html.parser')
 listOfDays = data2.body.table
 linkOfDays = listOfDays.find_all('a')[5:]
 dataPerDay = []
-# opName = "2019"
-# os.makedirs(opName, exist_ok=True)
-# if(len(dataPerDay)<1):
-#     for i in linkOfDays:
-#         dirc= i.text
-#         path = os.path.join(opName,dirc)
-#         os.makedirs(path ,exist_ok=True)
-#         urlhdf= url + dirc
-#         l= rq.get(urlhdf , auth=HTTPBasicAuth(username=uname, password=pswd ))
-#         page= bs(l.text, 'html.parser')
-#         Table= page.body.table
-#         linkPerDay= Table.find_all('a', href=lambda href: href and href.endswith('.HDF5'))[5:]
-#         dataPerDay.append(linkPerDay)
+opName = "2019"
+os.makedirs(opName, exist_ok=True)
+if(len(dataPerDay)<1):
+    for i in linkOfDays:
+        dirc= i.text
+        path = os.path.join(opName,dirc)
+        os.makedirs(path ,exist_ok=True)
+        urlhdf= url + dirc
+        l= rq.get(urlhdf , auth=HTTPBasicAuth(username=uname, password=pswd ))
+        page= bs(l.text, 'html.parser')
+        Table= page.body.table
+        linkPerDay= Table.find_all('a', href=lambda href: href and href.endswith('.HDF5'))[5:]
+        dataPerDay.append(linkPerDay)
 
-# print('1')
+print('1')
 
-# # if (myfile.is_file() == False):
-# for j in dataPerDay:
-#     for k in j:
-#         fileN = k["href"]
-#         for m in linkOfDays:
-#             fileurl = url + m.text + "/" + fileN
-#             hdfile = rq.get(fileurl, auth=HTTPBasicAuth(username=uname, password=pswd ))
-#             if hdfile.status_code == 200:
-#                 with open(f"2019/{m.text}/{fileN}", 'wb') as f:
-#                     f.write(hdfile.content)
+# if (myfile.is_file() == False):
+for j in dataPerDay:
+    for k in j:
+        fileN = k["href"]
+        for m in linkOfDays:
+            fileurl = url + m.text + "/" + fileN
+            hdfile = rq.get(fileurl, auth=HTTPBasicAuth(username=uname, password=pswd ))
+            if hdfile.status_code == 200:
+                with open(f"2019/{m.text}/{fileN}", 'wb') as f:
+                    f.write(hdfile.content)
 
-# print('2')
+print('2')
 
 
 
--- model.py
+++ model.py
@@ -5,19 +5,18 @@ import tensorflow as tf
 from keras import layers, models
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-def load_hdf5_file(file_path):
-    with hdf.File(file_path, 'r') as f:
-        dataset_key = 'Grid'
-        if dataset_key in f:  
-            data = f[dataset_key][:]
-            return data
+def load_hdf5_file(file_path, dataset_key):
+    f = hdf.File(file_path, 'r')
+    if dataset_key in f:  
+        data = f[dataset_key]
+        return data
         
 def process_multiple_hdf5_files(directory, dataset_key):
     all_data = []
     for root, dirs, files in os.walk(directory):
         for fil in files:
             if fil.endswith('.HDF5'):
-                file_path = os.path.join(root, dirs, fil)
+                file_path = os.path.join(root, fil)
                 print(f"Processing file: {file_path}")
                 data = load_hdf5_file(file_path, dataset_key)
                 if data is not None:
@@ -29,14 +28,20 @@ directory_path = '2019'
 dataset_key = 'Grid'
 alldata = process_multiple_hdf5_files(directory_path, dataset_key)
 
-X = np.array(alldata['Grid'])
-y = np.array(alldata['Grid/precipitation'])
+X = []
+y = []
+for i in alldata:
+    y.append(i)
+    X.append(i['precipitation'])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = np.array(X)
+y = np.array(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, train_size=0.4, random_state=42)
 
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+X_train = scaler.fit_transform(X_train.reshape(-1, X_train.shape[-1])).reshape(X_train.shape)
+X_test = scaler.transform(X_test.reshape(-1, X_test.shape[-1])).reshape(X_test.shape)
 
 model = models.Sequential([
     layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
