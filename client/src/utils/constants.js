735d2bb9dada8d544d95ebee36eb8ad6c68b9647
Harshvardhansingh19 <yobrobiru@gmail.com>
2024-10-06 02:06:47 +0530

    done build

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const HOST = import.meta.env.VITE_SERVER_URL;
+export const HOST = "https://backendauthfarmingo.vercel.app" ;
 
 export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
