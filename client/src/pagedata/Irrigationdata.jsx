fb1a3f0bcc1792f5eaf4fecee2497e8ac718ef84
krishnaGauss <goswamikrishna549@gmail.com>
2025-09-25 00:00:14 +0530

    some minor updates

--- client/src/pagedata/Irrigationdata.jsx
+++ client/src/pagedata/Irrigationdata.jsx
@@ -4,7 +4,9 @@ import React from 'react';
 const Irrigationdata = () => {
   return (
     <div>
+      <a href="https://66ec166812af5316eab69cd0--extraordinary-kulfi-82379a.netlify.app/" target="_blank">
       <h1 className="text-2xl font-bold">Irrigation Model</h1>
+      </a>
       <p className="mt-4">All input and data </p>
     </div>
   );
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,5 @@
-export const HOST = "https://backendauthfarmingo.vercel.app" ;
+// export const HOST = "https://backendauthfarmingo.vercel.app" ;
+export const HOST = "http://localhost:7600" ;
 
 export const AUTH_ROUTES = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTES}/signup`;
